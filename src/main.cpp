f7b1b566f21ed31fae1350879895a79b00a9855b
pn2222a <sneakylizard123_4@hotmail.com>
2025-11-10 20:34:19 -0800

    ooh fancy

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,14 @@ int ledPin = 13; // choose the pin for the LED
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT); // declare the LED pin as an OUTPUT
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int randomValue = random(50); // generate a random number between 0 and 99
+  Serial.println(randomValue);
+  
   digitalWrite(ledPin, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(randomValue); // wait for a random time
   digitalWrite(ledPin, LOW); // turn the LED off by making the voltage LOW
