14d584c05753f6e5f460a5d4e738cb6937126644
pn2222a <sneakylizard123_4@hotmail.com>
2025-11-10 19:41:14 -0800

    random

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int randomValue = random(100); // generate a random number between 0 and 99
-  if (randomValue < 50) {
-    digitalWrite(ledPin, HIGH); // turn the LED on
-  } else {
-    digitalWrite(ledPin, LOW); // turn the LED off
-  }
+  int randomValue = random(50); // generate a random number between 0 and 99
+  digitalWrite(ledPin, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(randomValue); // wait for a random time
+  digitalWrite(ledPin, LOW); // turn the LED off by making the voltage LOW
+  delay(randomValue); // wait for a random time
 }
